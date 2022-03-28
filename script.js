9e0854ab456a08e19b2752e329e49fbe73cd2508
jonathan almeida <jonathanalmeidajua@gmail.com>
2022-03-28 17:06:42 -0300

    erro add

--- script.js
+++ script.js
@@ -1,32 +1,97 @@
 const transactionsUl = document.querySelector('#transactions')
-console.log(transactionsUl)
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('#amount')
 
-const dummyTransactions = [
-    { id: 1, name: 'bolo de brigadeiro', amount: -20 },
-    { id: 2, name: 'Salario', amount: 300 },
-    { id: 3, name: 'Torta de Frango', amount: -10 },
-    { id: 4, name: 'Violão', amount: -150 }
-]
 
-const addTransactionIntoDOM = transaction => {
-    const operator = transaction.amount < 0 ? '-': '+';
-    const CSSClass = transaction.amount <0 ? 'minus':'plus';
-    const amountWithoutOperator = Math.abs(transaction.amount)
-    const li = document.createElement('li') 
-    li.classList.add(CSSClass)
-    li.innerHTML = `
-    ${transaction.name}<span>${operator}R$${amountWithoutOperator}</span><button class="delete-btn">x</button>
-    `
-     transactionsUl.prepend(li)
-    console.log(li)
+const localStorageTransactions = JSON.parse(localStorage
+    .getItem('transactions'))
+
+let transaction = localStorage
+.getItem('transactions')!== null ? localStorageTransactions : []
+
+const removeTransaction = ID => {
+    transaction = transaction
+    .filter(transaction => transaction.id !== ID)
+    init()
 }
 
-const updateBalanceValues = () =>{
-    const transactionsAmounts = dummyTransactions.map(transaction => transaction.amount)
+const addTransactionIntoDOM = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+';
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
+    const amountWithoutOperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+    ${transaction.name}
+    <span>${operator}R$${amountWithoutOperator}</span>
+    <button class="delete-btn" onClick="removeTransaction(${transaction.id})">x</button>
+    `
+    transactionsUl.prepend(li)
+
+}
+
+const updateBalanceValues = () => {
+    const transactionsAmounts = transaction
+        .map(transaction => transaction.amount)
+
+    const total = transactionsAmounts
+        .reduce((accumulator, transaction) => accumulator + transaction, 0)
+        .toFixed(2)
+
+    const income = transactionsAmounts
+        .filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+
+    const expense = Math.abs(transactionsAmounts
+        .filter(value => value < 0)
+        .reduce((accumulator, value) => accumulator + value, 0))
+        .toFixed(2)
+
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
 }
 
 const init = () => {
-    dummyTransactions.forEach(addTransactionIntoDOM)
+    transactionsUl.innerHTML = ''
+    transaction.forEach(addTransactionIntoDOM)
+    updateBalanceValues()
+}
+
+const updateLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transaction))
 }
 
 init()
+const generateID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const transactionName = inputTransactionName.value.trim()
+    const transactionAmount = inputTransactionAmount.value.trim()
+
+    if (transactionName === '' || transactionAmount === '') {
+        alert('Por favor, preeencha tanto o nome quanto o valor da transação')
+        return
+    }
+
+    const transaction = {
+        id: generateID(),
+        name: transactionName,
+        amount: Number(transactionAmount)
+    }
+
+    transactions.push(transaction)
+    
+    init()
+    updateLocalStorage()
+
+    inputTransactionName.value = ''
+    inputTransactionAmount.value = ''
+})
